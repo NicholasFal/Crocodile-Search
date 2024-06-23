bc8521c9f11c6d4789dbf99114c95f2c2917fafb
NicholasFal <67983932+NicholasFal@users.noreply.github.com>
2024-06-23 12:26:41 -0700

    Add files via upload

---
+++ Crocodile-Search/src/test/java/org/jointheleague/api/crocodile/Crocodile/Search/CrocodileSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.crocodile.Crocodile.Search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrocodileSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
